2cfe4076751e239c9a9037cb59b2e09eaa469a64
Zane Ravenholt <zravenholt@gmail.com>
2017-12-19 18:20:37 -0800

    reroute integration to correct file

--- marketo.js
+++ marketo.js
@@ -1,4 +1,4 @@
 'use strict';
 let datafire = require('datafire');
-let openapi = require('./marketoOAuth.json');
+let openapi = require('./integrations/marketo/marketoOAuth.json');
 module.exports = datafire.Integration.fromOpenAPI(openapi, 'marketo');
