b4867e0ae26433a5ae30571d211f9e3f3073fa43
Zane Ravenholt <zravenholt@gmail.com>
2017-12-19 18:26:36 -0800

    remove obsolete file

--- marketo.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-let datafire = require('datafire');
-let openapi = require('./integrations/marketo/marketoOAuth.json');
-module.exports = datafire.Integration.fromOpenAPI(openapi, 'marketo');
