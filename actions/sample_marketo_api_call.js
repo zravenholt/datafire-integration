3af78a585fa7e0cf40d830e6e644da5aebe18150
DataFire <info@datafire.io>
2017-12-21 11:45:14 -0800

    working oauth action, separate sample call to Rest API

--- actions/sample_marketo_api_call.js
+++ actions/sample_marketo_api_call.js
@@ -1,15 +1,17 @@
 "use strict";
 let datafire = require('datafire');
 
-let marketo = datafire.Integration.fromName('marketoOAuth').actions;
+let marketoOAuth = datafire.Integration.fromName('marketoOAuth').actions;
 let marketoRestAPI = datafire.Integration.fromName('marketoRestAPI').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
-    let responseOfIdentity = await marketo.identityUsingGET({
+    let responseOfIdentity = await marketoOAuth.identityUsingGET({
       client_id: "CLIENT ID",
       client_secret: "CLIENT SECRET",
       grant_type: "client_credentials",
     }, context);
-    return responseOfIdentity;
+    context.accounts.marketoRestAPI = {access_token: responseOfIdentity.access_token}
+    let result = await marketoRestAPI.getAllChannelsUsingGET({}, context);
+    return result;
   },
 });
