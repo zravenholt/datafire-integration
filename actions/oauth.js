e61d21b47e07bbdd1664c3ccd67a0345c84f05b4
DataFire <info@datafire.io>
2017-12-26 11:27:44 -0800

    Edit workspace

--- actions/oauth.js
+++ actions/oauth.js
@@ -6,8 +6,8 @@ let marketoRestAPI = datafire.Integration.fromName('marketoRestAPI').actions;
 module.exports = new datafire.Action({
   handler: async (input, context) => {
     let responseOfIdentity = await marketo.identityUsingGET({
-      client_id: "CLIENT ID",
-      client_secret: "CLIENT SECRET",
+      client_id: "1026b5b5-61f9-484a-8012-02fdc0978510",
+      client_secret: "L6gAfuwyZbWV8w87pIxgU1Ua8yDq6Mes",
       grant_type: "client_credentials",
     }, context);
     return responseOfIdentity;
